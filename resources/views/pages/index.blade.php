5dd1e58ead18df32811b783e5ef7ab7b53f77b30
Dominik Rusac <rusac.dominik@gmail.com>
2019-06-12 09:12:18 +0200

    Final hm

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome To Ventex Service Application</h1>
-        <p>This is application for service orders built using Laravel PHP Framework @ Ventex d.o.o.</p>
+        <h1>Welcome To Laravel Blog</h1>
+        <p>This is blog built using Laravel PHP Framework</p>
         @guest
-            <p>Please login to proceed</p>
+            <p>Please login or register</p>
             <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
         @endguest
     </div>
